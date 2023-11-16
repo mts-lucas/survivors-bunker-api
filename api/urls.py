4c5aa825ce607723870f63a30f6ebe16b1e0f4a2
Lucas Mateus <lmateus1067@outlook.com>
2023-11-16 12:49:52 -0300

    add medias

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -14,4 +16,5 @@ urlpatterns = [
     # path('search/', SearchView.as_view(), name='search'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = format_suffix_patterns(urlpatterns)
