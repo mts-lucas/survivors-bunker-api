83d4deca1824601102f2ebbc0ba9c110eef869b9
Lucas Mateus <lmateus1067@outlook.com>
2023-11-30 20:47:35 -0300

    add db

--- bunker/settings.py
+++ bunker/settings.py
@@ -91,10 +91,16 @@ WSGI_APPLICATION = 'bunker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.parse(config("DATABASE_URL"))
-}
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default=config('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
