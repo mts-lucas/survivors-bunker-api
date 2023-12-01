9ac9424d8aedb074ff50719d08cc8b4cad2f1c7f
Lucas Mateus <lmateus1067@outlook.com>
2023-11-30 21:20:51 -0300

    add

--- bunker/settings.py
+++ bunker/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # MEDIA_URL = '/bunker/api/v1/media/'
 
 # Default primary key field type
